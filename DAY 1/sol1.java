60f10b3cc21495798d5b87f527f07e0df001e866
narasimhan-2020 <lakshminarasimhan592@gmail.com>
2024-05-27 21:41:51 +0530

    1

---
+++ DAY 1/sol1.java
@@ -0,0 +1,14 @@
+
+class Compute {
+    
+    public int largest(int arr[], int n)
+    {
+        int max = 0;
+        for(int i=0;i<n;i++){
+            if(arr[i]>max){
+                max = arr[i];
+            }
+        }
+    return max;
+    }
+}
