f2ea62058e2878dd5f73555464b06afbaf9fc219
narasimhan-2020 <lakshminarasimhan592@gmail.com>
2024-07-05 23:13:13 +0530

    41

---
+++ DAY 41/sol41.java
@@ -0,0 +1,15 @@
+class Solution
+{
+    public int NthRoot(int n, int m)
+    {
+        // code here
+        int low=1,high=m;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(Math.pow(mid,n)==m) return mid;
+            else if(Math.pow(mid,n)<m) low=mid+1;
+            else high=mid-1;
+        }
+        return -1;
+    }
+}
