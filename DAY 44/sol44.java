7b606ee61aab71cc011b4fcef30f83b6dcce2cef
narasimhan-2020 <lakshminarasimhan592@gmail.com>
2024-07-07 23:14:57 +0530

    44

---
+++ DAY 44/sol44.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int minDays(int[] bloomDay, int m, int k) {
+        long val = (long) m * k;
+        int n = bloomDay.length; // Size of the array
+        if (val > n) return -1; // Impossible case.
+        
+        // Find maximum and minimum:
+        int mini = Integer.MAX_VALUE, maxi = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            mini = Math.min(mini, bloomDay[i]);
+            maxi = Math.max(maxi, bloomDay[i]);
+        }
+
+        // Apply binary search:
+        int low = mini, high = maxi;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (possible(bloomDay, mid, m, k)) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+
+    public static boolean possible(int[] bloomDay, int day, int m, int k) {
+        int n = bloomDay.length; // Size of the array
+        int cnt = 0;
+        int noOfB = 0;
+        // Count the number of bouquets:
+        for (int i = 0; i < n; i++) {
+            if (bloomDay[i] <= day) {
+                cnt++;
+                if (cnt == k) {
+                    noOfB++;
+                    cnt = 0;
+                }
+            } else {
+                cnt = 0;
+            }
+        }
+        return noOfB >= m;
+    }
+}
