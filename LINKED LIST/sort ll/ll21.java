fb5068454fba5f68ec809af3a2706c43ac5853e6
narasimhan-2020 <lakshminarasimhan592@gmail.com>
2024-08-29 05:05:00 +0530

    ll21

---
+++ LINKED LIST/sort ll/ll21.java
@@ -0,0 +1,63 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if(head ==null || head.next == null) return head; 
+        ListNode middle = findmid(head);
+        ListNode left = head;
+        ListNode right = middle.next;
+        middle.next =null;
+
+        left = sortList(left);
+        right = sortList(right);
+
+        return mergeTwoSortedLinkedLists(left, right);
+    }
+    public ListNode findmid(ListNode head){
+         if (head == null || head.next == null) {
+        return head;
+    }
+
+        ListNode slow  = head;
+        ListNode fast = head.next;
+
+        while(fast!=null && fast.next != null){
+            slow = slow.next;
+            fast= fast.next.next;
+        }
+        return slow;
+    }
+    ListNode mergeTwoSortedLinkedLists(ListNode list1, ListNode list2) {
+    
+    ListNode dummyNode = new ListNode(-1);
+    ListNode temp = dummyNode;
+
+    while (list1 != null && list2 != null) {
+       
+        if (list1.val <= list2.val) {
+            temp.next = list1;
+            list1 = list1.next;
+        } else {
+            temp.next = list2;
+            list2 = list2.next;
+        }
+      
+        temp = temp.next; 
+    }
+    if (list1 != null) {
+        temp.next = list1;
+    } else {
+        temp.next = list2;
+    }
+   
+    return dummyNode.next;
+}
+}
