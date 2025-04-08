91cc8b8389d757fd44494e960984da83a871275c
narasimhan-lakshmi <lakshminarasimhan592@gmail.com>
2025-04-08 21:37:49 +0530

    tuf 4

---
+++ Heaps/kthLargestInStream.java
@@ -0,0 +1,64 @@
+// LC 703
+
+class KthLargest {
+    private PriorityQueue<Integer>pq;
+    private int k;
+
+    public KthLargest(int k, int[] nums) {
+        this.k =  k;
+         pq = new PriorityQueue<>();
+
+         for(int num : nums){
+            pq.add(num);
+            if(pq.size()>k) pq.poll();
+         }
+    }
+    
+    public int add(int val) {
+        pq.add(val);
+        if(pq.size()>k){
+            pq.poll();
+        }
+        return pq.peek();
+    }
+}
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.add(val);
+ * 
+ * Example 1:
+
+Input:
+["KthLargest", "add", "add", "add", "add", "add"]
+[[3, [4, 5, 8, 2]], [3], [5], [10], [9], [4]]
+
+Output: [null, 4, 5, 5, 8, 8]
+
+Explanation:
+
+KthLargest kthLargest = new KthLargest(3, [4, 5, 8, 2]);
+kthLargest.add(3); // return 4
+kthLargest.add(5); // return 5
+kthLargest.add(10); // return 5
+kthLargest.add(9); // return 8
+kthLargest.add(4); // return 8
+
+Example 2:
+
+Input:
+["KthLargest", "add", "add", "add", "add"]
+[[4, [7, 7, 7, 7, 8, 3]], [2], [10], [9], [9]]
+
+Output: [null, 7, 7, 7, 8]
+
+Explanation:
+
+KthLargest kthLargest = new KthLargest(4, [7, 7, 7, 7, 8, 3]);
+kthLargest.add(2); // return 7
+kthLargest.add(10); // return 7
+kthLargest.add(9); // return 7
+kthLargest.add(9); // return 8
+
+ */
