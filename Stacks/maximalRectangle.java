7e0f719e11c59dabd873488cc7e3a4eefaa29b23
narasimhan-lakshmi <lakshminarasimhan592@gmail.com>
2025-04-10 22:50:45 +0530

    tuf14

---
+++ Stacks/maximalRectangle.java
@@ -0,0 +1,60 @@
+// LC 85
+
+class Solution {
+   
+    public int maximalRectangle(char[][] matrix) {
+        if (matrix == null || matrix.length == 0) {
+            return 0;
+        }
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int maxArea = 0;
+        int[] heights = new int[m];  
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == '0') {
+                    heights[j] = 0;
+                } else {
+                    heights[j]++;
+                }
+            }
+           
+            maxArea = Math.max(maxArea, largestRectangleArea(heights));
+        }
+
+        return maxArea;
+    }
+
+    
+    public int largestRectangleArea(int[] heights) {
+        Stack<Integer> stack = new Stack<>();
+        int maxArea = 0;
+        int n = heights.length;
+
+        for (int i = 0; i <= n; i++) {
+            int h = (i == n) ? 0 : heights[i];
+            while (!stack.isEmpty() && heights[stack.peek()] > h) {
+                int height = heights[stack.pop()];
+                int startIndex = stack.isEmpty() ? -1 : stack.peek();
+                maxArea = Math.max(maxArea, height * (i - startIndex - 1));
+            }
+            stack.push(i);
+        }
+
+        return maxArea;
+    }
+}
+/*
+Input: matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]
+Output: 6
+Explanation: The maximal rectangle is shown in the above picture.
+Example 2:
+
+Input: matrix = [["0"]]
+Output: 0
+Example 3:
+
+Input: matrix = [["1"]]
+Output: 1
+*/
